db86eb3c88a74f05f48e97ad0c9baf0aee29c1c8
chester <gaoxiangyu@inspur.com>
2026-02-10 16:34:28 +0800

    Bundle MSVC runtime DLLs in Windows build

--- buildexe.py
+++ buildexe.py
@@ -1,5 +1,6 @@
 #-*- coding: utf-8 -*-
 
+import os
 import sys
 from distutils.core import setup
 import py2exe
@@ -11,12 +12,24 @@ dll_excludes = ["msvcm90.dll", "msvcp90.dll", "msvcr90.dll"]
 if len(sys.argv) == 1:
     sys.argv.append("py2exe")
 
+def _runtime_dlls():
+    system_root = os.environ.get("SystemRoot", r"C:\Windows")
+    candidates = [
+        os.path.join(system_root, "System32", "vcruntime140.dll"),
+        os.path.join(system_root, "System32", "vcruntime140_1.dll"),
+        os.path.join(system_root, "System32", "msvcp140.dll"),
+        os.path.join(system_root, "System32", "msvcp140_1.dll"),
+        os.path.join(system_root, "System32", "msvcp140_2.dll"),
+    ]
+    return [path for path in candidates if os.path.exists(path)]
+
 setup(  version="0.1",
         description = "Arduino Hex Uploader",
         name = "arduloader.exe",
         author = "Apache",
         packages = [],
         py_modules = [],
+        data_files = [(".", _runtime_dlls())],
         zipfile = None,
         windows = [{"script":"main.py", "icon_resources":[(1, "./icons/main/logo.ico")], "dest_base":"arduloader"}],
 		options = {   "py2exe":
